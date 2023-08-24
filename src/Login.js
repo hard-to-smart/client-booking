5ae63c704c348ce3e3690e296191ba9df5460004
hard-to-smart <2403punjabidivya@gmail.com>
2023-08-24 08:03:30 +0530

    poorane

--- src/Login.js
+++ src/Login.js
@@ -52,14 +52,14 @@ const Login = () => {
                   </div>
                   {loginError && <p style={{ color: 'red' }}>{loginError}</p>}
                   <div className="flex items-center justify-between">
-                      <div className="flex items-start">
-                          {/* <div className="flex items-center h-5">
+                      {/* <div className="flex items-start">
+                           <div className="flex items-center h-5">
                             <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                          </div> */}
+                          </div> 
                           <div className="ml-3 text-sm">
                             <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                           </div>
-                      </div>
+                      </div> */}
                       <Link to='/Forgotpass' className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</Link>
                   </div>
                   <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={handleLoginSubmit}>Sign in</button>
